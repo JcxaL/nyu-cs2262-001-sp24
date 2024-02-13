321e062ae0ae344c633e550b36fefeabac99ca4e
Jiangchen Li <jccl@alvins-air.mynetworksettings.com>
2024-02-13 00:05:06 -0500

    lab2

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,11 +1,17 @@
 from flask import Flask
+import datetime
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
+# /time
+@app.route('/time')
+def current_time():
+    now = datetime.datetime.now()
+    time_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    return time_string
 
 app.run(host='0.0.0.0',
         port=8080,
